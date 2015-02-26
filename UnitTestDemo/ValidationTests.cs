b4ec6ad74d5a54fc40595d6c01701ffa3253e481
andy.hopper@soltech.net <andy.hopper@soltech.net>
2015-02-26 11:46:24 -0500

    Added the object validation tests for our Julia Childs moment.

---
+++ UnitTestDemo/ValidationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SolTech.Demos.UnitTesting
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void VerifyThatEmptyGuidsAreInvalid()
+        {
+            var item = new MyObject { Id = Guid.Empty, Created = DateTime.Now, Name = "Valid" };
+            var results = new List<ValidationResult>();
+            Assert.False(Validator.TryValidateObject(item, new ValidationContext(item), results));
+        }
+
+        [Fact]
+        public void VerifyThatFutureCreatedDatesAreInvalid()
+        {
+            var item = new MyObject { Id = Guid.NewGuid(), Created = DateTime.Now.AddDays(1), Name = "Valid" };
+            var results = new List<ValidationResult>();
+            Assert.False(Validator.TryValidateObject(item, new ValidationContext(item), results));
+        }
+    }
+}
