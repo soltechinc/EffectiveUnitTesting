6dd4d6362bdf2b8bb6e872ce1513bbf72a772a1c
andy.hopper@soltech.net <andy.hopper@soltech.net>
2015-02-26 11:01:50 -0500

    Removed ValidationTests in preparation for the demo Added code coverage report generator to solution Added comments, per-user COM registration to the code coverage script

--- UnitTestDemo/runCoverage.cmd
+++ UnitTestDemo/runCoverage.cmd
@@ -14,12 +14,41 @@ SET "XUnitConsolePath=%CurDir%packages\xunit.runners.2.0.0-rc3-build2880\tools\x
 SET "OpenCoverPath=%CurDir%packages\OpenCover.4.5.3723\OpenCover.Console.exe"
 SET "ReportGeneratorPath=%CurDir%packages\ReportGenerator.2.1.1.0\ReportGenerator.exe"
 
-"%OpenCoverPath%" "-output:%CoverageReportName%" "-target:%XUnitConsolePath%" "-targetdir:%CurDir%bin\Debug" "-targetargs:""%UnitTestFileName%"" -noshadow" "-filter:+[*]* -[xunit*]* -[%UnitTestAssembly%]*"
+:: Some explanations:
+:: -register:user
+:: OpenCover uses a the .NET Framework's COM-based extensibility to profile
+:: the code. This switch tells OpenCover to register this COM library for the
+:: duration of the run. Since global COM  registration requires elevated
+:: privileges, the 'user' bit tells it to register it in the HKEY_CURRENT_USER
+:: hive (does NOT need elevation).
+::
+:: -output:<file path>
+:: The results of the coverage pass
+::
+:: -target:<file path>
+:: The executable that will be run for the pass
+::
+:: -targetdir:<folder path>
+:: The working directory (used for finding PDBs).
+::
+:: -targetargs:""<arguments>""
+:: The arguments passed to the target. Since we're using XUnit, this
+:: will be a path to the unit test assembly.
+::
+:: -filter:<filter expressions>
+:: An expression telling OpenCover what assemblies/classes to include in
+:: its analysis. It uses a +/-[<AssemblyNameExpression>]<ClassNameExpression>
+:: format that supports wildcard globbing.
+::
+"%OpenCoverPath%" -register:user "-output:%CoverageReportName%" "-target:%XUnitConsolePath%" "-targetdir:%CurDir%bin\Debug" "-targetargs:""%UnitTestFileName%"" -noshadow" "-filter:+[*]* -[xunit*]* -[%UnitTestAssembly%]*"
 IF NOT ERRORLEVEL 0 (
 	ECHO Unable to perform code coverage - Error %ERRORLEVEL%.
 	SET RESULT=-1
 	GOTO END
 )
+::
+:: Generate a nicely-formatted set of HTML documents with the results
+::
 "%ReportGeneratorPath%" "-reports:%CoverageReportName%" "-targetdir:%ReportDirectory%"
 IF NOT ERRORLEVEL 0 (
 	ECHO Unable to generate report - Error %ERRORLEVEL%.
