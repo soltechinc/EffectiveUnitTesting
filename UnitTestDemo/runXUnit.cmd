69c9e97245925f77982875590d99b1555851e34d
andy.hopper@soltech.net <andy.hopper@soltech.net>
2015-03-05 09:41:28 -0500

    Made sure XUnit runners would be available by adding it to the project packages.config Fixed misspelling in runXUnit script

--- UnitTestDemo/runXUnit.cmd
+++ UnitTestDemo/runXUnit.cmd
@@ -1,6 +1,6 @@
 @echo off
-SET "CurDur=%~dp0"
-for /d %%i in ("%CurDur%packages\xunit.runners.2.0.0*") do (
+SET "CurDir=%~dp0"
+for /d %%i in ("%CirDur%packages\xunit.runners.2.0.0*") do (
 	for %%j in ("%%i\tools\xunit.console.exe") do set "XUnitPath=%%j"
 )
 %XUnitPath% %*
