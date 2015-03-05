55c601500748761383e3b256d85fa93d9bcf035c
andy.hopper@soltech.net <andy.hopper@soltech.net>
2015-03-05 09:50:08 -0500

    Argh! Fixed mispelling of the mispelling

--- UnitTestDemo/runXUnit.cmd
+++ UnitTestDemo/runXUnit.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET "CurDir=%~dp0"
-for /d %%i in ("%CirDur%packages\xunit.runners.2.0.0*") do (
+for /d %%i in ("%CurDir%packages\xunit.runners.2.0.0*") do (
 	for %%j in ("%%i\tools\xunit.console.exe") do set "XUnitPath=%%j"
 )
 %XUnitPath% %*
